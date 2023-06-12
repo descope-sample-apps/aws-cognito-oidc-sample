0ceea3fb5e9bcba74f5d6fdb9601738a324879d8
Kevin Gao <kevinjamesgao@gmail.com>
2023-06-12 13:41:45 -0700

    added README

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ import { AuthProvider } from "@descope/react-sdk";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider
-    projectId={process.env.DESCOPE_PROJECT_ID || "P2PD8H2nZbPk5cSJfChsc00ZCi0w"}
-  >
+  <AuthProvider projectId={process.env.DESCOPE_PROJECT_ID}>
     <BrowserRouter>
       <React.StrictMode>
         <Routes>
